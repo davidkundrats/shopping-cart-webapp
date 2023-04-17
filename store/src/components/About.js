e672463e220134edaf710d47caad02adb7a42673
davidkundrats <davidkundrats@gmail.com>
2023-04-17 01:22:48 -0400

    deployed build

--- store/src/components/About.js
+++ store/src/components/About.js
@@ -12,6 +12,16 @@ export default function About() {
           shot on a Leica m2 or Olympus XA. All sizes for orders are 5x7 and
           will have above standard contrast.{" "}
         </p>
+        <p>
+          For any questions or inquiries, please feel free to contact me at{" "}
+          <a href="mailto:daves-prints@outlook.com">here</a>.
+          You can also check out my work on Instagram {" "}
+          <a href="https://www.instagram.com/bigstripedpants" target="_blank" rel="noopener noreferrer">
+            here
+          </a>
+          .
+          Thanks :)
+        </p>
       </div>
     </div>
   );
